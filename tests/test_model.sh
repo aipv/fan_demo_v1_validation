39b0ac5fd529dbb434a108dbe66963df366ed886
Qing Zhou <qingzhou.berlin@gmail.com>
2026-06-23 13:02:14 +0200

    test.model

--- tests/test_model.sh
+++ tests/test_model.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 echo "Building test_model..."
 gcc -c ../dataset/model/model.c -o model.o -lm
 gcc -c test_model.c -o test_model.o -lm
-gcc model.o test_model.o -o test_model -lm
+gcc model.o test_model.o -o test.model -lm
 
 echo "Running test_model..."
-./test_model
+./test.model
